c63bba656ed7e831524a8a4782048649087c4404
Payal <kshirsagarpayal185@gmail.com>
2026-05-17 15:52:26 +0530

    Add multiple variables for AWS and DB configuration

--- Terraform/depends_on/variable.tf
+++ Terraform/depends_on/variable.tf
@@ -1,1 +1,48 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "s3_bucket_name" {
+  type = string
+}
+
+variable "db_storage" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_engine_version" {
+  type = string
+}
+
+variable "db_engine" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_instance_type" {
+  type = string
+}
+
+variable "db_skip_snapshot" {
+  type = string
+}
+
+variable "aws_instance_type" {
+  type = string
+}
+
+variable "aws_ami" {
+  type = string
+}
 
