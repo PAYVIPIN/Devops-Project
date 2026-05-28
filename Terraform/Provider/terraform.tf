bbebf02a119a8f119fc44d27b15adce1982bcfac
Payal <kshirsagarpayal185@gmail.com>
2026-05-28 17:02:17 +0530

    Modify required providers and versions in terraform.tf
    Updated required providers and their versions in terraform.tf

--- Terraform/Provider/terraform.tf
+++ Terraform/Provider/terraform.tf
@@ -1,8 +1,21 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
+    required_version = "= 1.14.4"
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "6.46.0"
+        }
+        http = {
+            source = "hashicorp/http"
+            version = "3.6.0"
+        }
+        random = {
+            source = "hashicorp/random"
+            version = "3.9.0"
+        }
+        local = {
+            source = "hashicorp/local"
+            version = "2.9.0"
+        }
     }
-  }
 }
